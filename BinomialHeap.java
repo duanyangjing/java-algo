8c1f39e933cc31a7a877a9a2b8a67605b163951c
duanyangjing <gmjingdy@gmail.com>
2017-06-14 20:25:11 -0500

    Add method signature

--- BinomialHeap.java
+++ BinomialHeap.java
@@ -3,18 +3,55 @@ import java.util.*;
 public class BinomialHeap {
 	
 	class BinomialTreeNode {
-		int data;
+		int key;
 		BinomialTreeNode leftChild;
 		BinomialTreeNode rightSib;
 		BinomialTreeNode parent;
 		int degree;
+
+		public BinomialTreeNode(int key) {
+			leftChild = null;
+			rightSib = null;
+			parent = null;
+			degree = 0;
+		}
+
+
 	}
 
-	class BinomialTree {
-		BinomialTreeNode root;
-		int degree;
+	BinomialTreeNode root;
+	int size;
+
+	public BinomialHeap() {
+		this.root = null;
+		this.size = 0;
 	}
 
-	List<BinomialTree> rootList;
-	int size
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public BinomialHeap insert(int key) {
+		return null;
+	}
+
+	public int findMin() {
+		return 0;
+	}
+
+	public int extractMin() {
+		return 0;
+	}
+
+	public BinomialHeap union(BinomialHeap heap1, BinomialHeap heap2) {
+		return null;
+	}
+
+	public BinomialHeap decreaseKey(int key, int newKey) {
+		return null;
+	}
+
+	public BinomialHeap delete(int key) {
+		return null;
+	}
 }
